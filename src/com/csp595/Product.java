07d0aaf369d44d5115659e0cef52af663aa3534b
Cheths <cbhaskar@hawk.iit.edu>
2016-11-05 02:14:11 -0500

    Initial Commit

---
+++ src/com/csp595/Product.java
@@ -0,0 +1,64 @@
+package com.csp595;
+
+public class Product {
+	
+	private String  id;
+	private String name;
+	private String category;
+	private double  price;
+	private double manufacturer;
+	private String discount;
+	private String condition;
+	private String image;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(double manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getDiscount() {
+		return discount;
+	}
+	public void setDiscount(String discount) {
+		this.discount = discount;
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
