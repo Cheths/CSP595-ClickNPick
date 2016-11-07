d65d9c1924c61a2d3227c2debdc79b81b6c56a4c
Cheths <cbhaskar@hawk.iit.edu>
2016-11-07 00:29:58 -0600

    Test Commit

--- src/com/csp595/utilities/SaxParserProductXMLdataStore.java
+++ src/com/csp595/utilities/SaxParserProductXMLdataStore.java
@@ -22,7 +22,6 @@ public class SaxParserProductXMLdataStore extends DefaultHandler {
 	private static List<Product> productList = new ArrayList<Product>();
 	private String elementValueRead;
 	private static Map<String, Product> productHashMap = new HashMap<String, Product>();
-		private int i;
 
 	public Product getProduct() {
 		return product;
