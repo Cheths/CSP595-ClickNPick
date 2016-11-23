3369ec9811bdbb2326d81082f3d662f3087e0e91
sudhindramanohar <sbellurvasudeva@hawk.iit.edu>
2016-11-22 18:10:29 -0600

    updated insert script to include gender column

--- Scripts/ClickNPick.sql
+++ Scripts/ClickNPick.sql
@@ -65,8 +65,8 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Sample Product Data*/
-INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`) VALUES (1,'Round Neck','Shirt','0',10,'Wrangler',1,'0',NULL,NULL);
-INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`) VALUES (2,'V Neck','Shirt','0',12,'Wrangler',2,'0',NULL,NULL);
-INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`) VALUES (3,'Slim Fit','Jeans','0',28,'Wrangler',2,'0',NULL,NULL);
-INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`) VALUES (4,'Low Waist','Jeans','0',35,'Denim',2,'0',NULL,NULL);
-INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`) VALUES (5,'Pull Over','Sweaters','0',22,'Addidas',3,'0',NULL,NULL);
+INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`,`gender`) VALUES (1,'Round Neck','Shirt','0',10,'Wrangler',1,'0',"themes/images/products/shirts5.jpg",NULL,NULL);
+INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`,`gender`) VALUES (2,'V Neck','Shirt','0',12,'Wrangler',2,'0',"themes/images/products/shirts6.jpg",NULL,NULL);
+INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`,`gender`) VALUES (3,'Slim Fit','Jeans','0',28,'Wrangler',2,'0',NULL,NULL,NULL);
+INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`,`gender`) VALUES (4,'Low Waist','Jeans','0',35,'Denim',2,'0',NULL,NULL,NULL);
+INSERT INTO `product` (`id`,`name`,`category`,`type`,`price`,`manufacturer`,`discount`,`condition`,`image`,`description`,`gender`) VALUES (5,'Pull Over','Sweaters','0',22,'Addidas',3,'0',NULL,NULL,NULL);
